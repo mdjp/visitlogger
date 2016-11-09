17950cafed365df9e57a65f7b803ed47f5e66bcf
Matthew Paradis <matthew.paradis@bbc.co.uk>
2016-11-09 14:41:52 +0000

    initial

---
+++ client/app.js
@@ -0,0 +1,42 @@
+(function(window) {
+    'use strict';
+
+    function initialise() {
+        var VisitLogger = {};
+        VisitLogger.server = 'SERVER';
+        VisitLogger.cookie = Cookies.get('venueexplorer');
+        if (!VisitLogger.cookie) {
+            Cookies.set('venueexplorer', uuid(), {
+                expires: 7
+            });
+        }
+
+
+        VisitLogger.logvisit = function(campaign, key, value) {
+            var data = {
+                key: key,
+                value: value,
+                campaign: campaign,
+                uid: VisitLogger.cookie
+            };
+            $.post(VisitLogger.server, data)
+        }
+
+        return VisitLogger;
+    }
+
+    if (typeof(VisitLogger) === 'undefined') {
+
+        window.VisitLogger = initialise();
+    }
+
+    function uuid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    }
+})(window);
---
+++ server/app.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const requestIp = require('request-ip');
+const http = require('http');
+const bodyParser = require('body-parser');
+const loader = require('json-load');
+const express = require('express');
+const Datastore = require('nedb');
+const cors = require('cors');
+const config = loader('config/main.json');
+const app = express();
+const db = new Datastore({
+  filename: config.dbpath,
+  autoload: true
+});
+
+
+function logvisit(req) {
+  const data = req.body;
+  const ipaddr = requestIp.getClientIp(req);
+  const date = new Date();
+  const timestamp = Math.round(date.getTime() / 1000);
+  const visit = {
+    ts: timestamp,
+    uid: data.uid,
+    key: data.key,
+    value: data.value,
+    campaign: data.campaign,
+    ip: ipaddr,
+    page: req.headers.referer
+  };
+  db.insert(visit);
+}
+
+function validatepost(data) {
+  if (data.key && data.value && data.campaign && data.uid) {
+    return true;
+  }
+  return false;
+}
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cors());
+
+http.createServer(app).listen(config.port);
+
+app.get('/', (req, res) => {
+  res.send(config.appname);
+});
+
+app.post('/logvisit', (req, res) => {
+  if (validatepost(req.body)) {
+    logvisit(req);
+    res.send('OK');
+  } else {
+    res.send('ERROR');
+  }
+});
+
